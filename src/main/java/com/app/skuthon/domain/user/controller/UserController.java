6e243e99f5c6fad20a4de83d514aa2436f98721c
{오지우} <ojing0116@skuniv.ac.kr>
2026-07-07 10:51:41 +0900

    :recycle: Refactor: BaseResponse 응답 감싸기

--- src/main/java/com/app/skuthon/domain/user/controller/UserController.java
+++ src/main/java/com/app/skuthon/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.app.skuthon.domain.user.dto.request.SignUpRequest;
 import com.app.skuthon.domain.user.dto.response.SignUpResponse;
 import com.app.skuthon.domain.user.entity.User;
 import com.app.skuthon.domain.user.service.UserService;
+import com.app.skuthon.global.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signup(@RequestBody SignUpRequest request) {
+    public ResponseEntity<BaseResponse<SignUpResponse>> signup(@RequestBody SignUpRequest request) {
         // UserService의 signup 메서드를 호출 (loginId, password, nickname 전달)
         userService.signup(request.getLoginId(), request.getPassword(), request.getNickname());
 
@@ -27,15 +28,17 @@ public class UserController {
         User user = userService.login(request.getLoginId(), request.getPassword());
 
         SignUpResponse response = new SignUpResponse(user.getId(), user.getNickname(), user.getPoints());
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(BaseResponse.success(201, "회원가입이 완료되었습니다.", response));
     }
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<SignUpResponse> login(@RequestBody SignUpRequest request) {
+    public ResponseEntity<BaseResponse<SignUpResponse>> login(@RequestBody SignUpRequest request) {
         User user = userService.login(request.getLoginId(), request.getPassword());
 
         SignUpResponse response = new SignUpResponse(user.getId(), user.getNickname(), user.getPoints());
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(BaseResponse.success(response));
     }
 }
